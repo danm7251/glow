48e9bee3e53e4b623d7728053f78fc02816051b5
Daniel Maynard <danielmaynard7@outlook.com>
2025-12-05 01:14:27 +0000

    Added some basic functions

--- src/player.rs
+++ src/player.rs
@@ -1,3 +1,5 @@
+use crate::audio::AudioEngine;
+
 pub enum PlaybackState {
     Playing { id: usize },
     Paused { id: usize },
@@ -5,17 +7,25 @@ pub enum PlaybackState {
 }
 
 pub struct Player {
+    audio: AudioEngine,
     state: PlaybackState,
+    volume: u8,
 }
 
 impl Player {
     pub fn new() -> Self {
         Self {
+            audio: AudioEngine::new(),
             state: PlaybackState::Stopped,
+            volume: 100,
         }
     }
 
     pub fn state(&self) -> &PlaybackState {
         &self.state
+    }
+
+    pub fn volume(&self) -> u8 {
+        self.volume
     }
 }
